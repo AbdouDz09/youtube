7b1f0f7e43d70427de0ae638d60c9c37e6afc19c
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2018-12-05 18:49:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,44 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const adminprefix = "Dz!";
-const developers = ['472413769700474901'];
-
-console.log("BaRooooooD");
-
 client.on('ready', () => {
-    console.log(Logged as ${client.user.tag})
-})
-
-
-client.on('message', message => {
-    var argresult = message.content.split().slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(تم تغيير البلاينق الى   ${argresult})
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(تَم تغيير الواتشينق الى   ${argresult})
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(تَم تغيير الليسينينق الى   ${argresult})
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(تم تغييرك حالتك بالتويتش الى   ${argresult})
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(جاري تغيير الأسم لـ ..${argresult} )
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(جاري تغيير الأفتار... :);
-}
+  console.log(Logged in as ${client.user.tag}!);
 });
 
-client.login(process.env.BOT_TOKEN);
+const prefix = "$"
+var adminprefix = '$$'
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '441316969870786581') return;
+
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(:white_check_mark:  : ${argresult})
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(:white_check_mark:  : ${argresult})
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(:white_check_mark:  : ${argresult})
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001%22);
+    message.channel.sendMessage(:white_check_mark:  : ${argresult})
+}
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(Changing The Name To ..${argresult})
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(Changing The Avatar To :${argresult});
+  }
+});
+
+client.login("token");
